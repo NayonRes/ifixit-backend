df8113ea346e839ef6212085c4eb9538ccf4b9e5
Jewel Rana <jewelrana.dev@newroztech.com>
2024-11-01 23:37:08 +0600

    Parent ID added in branches

--- routes/api/v1/deviceModel.js
+++ routes/api/v1/deviceModel.js
@@ -12,15 +12,15 @@ const {isAuthenticatedUser, hasPermission} = require("../../../middleware/auth")
 let router = express.Router();
 
 router.route("/device/model")
-    .get(isAuthenticatedUser, hasPermission('device-model-list'), index)
-    .post(isAuthenticatedUser, hasPermission('device-model-create'), store);
+    .get(isAuthenticatedUser, hasPermission('model-list'), index)
+    .post(isAuthenticatedUser, hasPermission('model-create'), store);
 
 router.route("/device/model/dropdown")
     .get(isAuthenticatedUser, dropdown);
 
 router.route("/device/model/:id")
-    .get(isAuthenticatedUser, hasPermission('device-model-show'), show)
+    .get(isAuthenticatedUser, hasPermission('model-show'), show)
     .put(isAuthenticatedUser, hasPermission('device-model-update'), update)
-    .delete(isAuthenticatedUser, hasPermission('device-model-action'), remove);
+    .delete(isAuthenticatedUser, hasPermission('model-action'), remove);
 
 module.exports = router;
